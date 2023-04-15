4f751ebef726a43a6f61d82e62a76dfd06306439
SwarnaSkyla <117882181+SwarnaSkyla@users.noreply.github.com>
2023-04-15 19:57:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 //creating a reference of players
-const team=players;
+const team = players;
 //copy of players
-const team1=players.splice();
+const team1 = players.splice();
 //object copy
-const cap1=Object.assign({},person);
+const cap1 = Object.assign({},person);
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// window.players = players;
+// window.person = person;
+// window.team = team;
+// window.team1 = team1;
+// window.cap1 = cap1;
